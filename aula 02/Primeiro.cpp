21a5faea0f088efacfb0b97f7f4f11788343e42d
Manuela Araújo <mxnuelafba@gmail.com>
2024-01-03 11:50:21 -0300

    first commit

---
+++ aula 02/Primeiro.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // Incluindo a biblioteca
+
+using namespace std; // Dispensa o uso de std::cout, std::endl
+
+int main() // Cabeçalho da função
+{
+	cout << "Bem vindo ao C++"; // Exibir mensagem
+	cout << endl; // Inserir uma nova linha
+	cout << "Meu primeiro programa!" << endl; // Outra mensagem
+
+	return 0; // Encerra a função main()
+}
