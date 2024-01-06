693c7225714522c29cfd745da5e636940e050990
Manuela Araújo <mxnuelafba@gmail.com>
2024-01-06 20:18:39 -0300

    exercício 1

---
+++ aula 04/ex01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void Sorria(); // protótipo
+
+int main()
+{
+	Sorria(); Sorria(); Sorria(); Sorria();
+    cout << endl;
+    Sorria(); Sorria();
+    cout << endl;
+    Sorria();
+	
+	return 0;
+}
+
+void Sorria() {
+    cout << "Sorria! ";
+}
