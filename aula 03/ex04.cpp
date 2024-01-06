38d2cedee4054acda804e87530467f4ceba73737
Manuela Araújo <mxnuelafba@gmail.com>
2024-01-06 19:59:13 -0300

    exercício 4

---
+++ aula 03/ex04.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int medida;
+    medida = 10;
+
+    cout << "Medida: " << medida << endl;
+    cout << "2x medida: " << medida * 2 << endl;
+    cout << "Medida ao quadrado: " << medida * medida;
+	
+	return 0;
+}
