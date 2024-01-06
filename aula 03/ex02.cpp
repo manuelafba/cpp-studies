e9f3f80ac64100012aa39addc5834c8785980d74
Manuela Araújo <mxnuelafba@gmail.com>
2024-01-06 18:37:18 -0300

    exercício 2

---
+++ aula 03/ex02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Que horas são? ";
+
+    int horas, minutos;
+    char pontos;
+
+    cin >> horas >> pontos >> minutos;
+
+    cout << "Horas: " << horas << endl;
+    cout << "Minutos: " << minutos;
+}
