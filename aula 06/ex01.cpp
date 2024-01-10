4a3c7b89a321cbd683e486267d06b197ca17fd4b
Manuela Araújo <mxnuelafba@gmail.com>
2024-01-10 20:37:19 -0300

    exercício 1

---
+++ aula 06/ex01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define ZERO 0 
+
+int main()
+{
+    int total = ZERO;
+    cout << "Total incial = " << total << " (ZERO)\n";
+    total = 50;
+    cout << "Depois da atribuição total = " << total << endl;
+    return ZERO;
+
+}
