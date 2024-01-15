dace68f88f7fdf1ef01ec2ad357eb2f2c4f5cf8a
Manuela Araújo <mxnuelafba@gmail.com>
2024-01-15 18:04:26 -0300

    exercício 4

---
+++ aula 07/ex04.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Morango = \t20 unidades" << endl;
+    cout << "Melancia = \t50 unidades" << endl;
+    cout << "Ameixa = \t30 unidades" << endl;
+
+	return 0;
+}
