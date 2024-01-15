137fd2c58bdad91e4f5aa7b212c91407277d50d0
Manuela Araújo <mxnuelafba@gmail.com>
2024-01-15 17:35:46 -0300

    exercício 2

--- aula 07/ex02.cpp
+++ aula 07/ex02.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void beep();
-int  lerSenha();
+void alerta();
+int lerSenha();
 
 int main()
 {
-	cout << "Iniciando com um som..." << endl;
-	beep();
+	alerta();
+	cout << "Iniciando com um som...";
 	cout << "Digite sua senha: ______\b\b\b\b\b\b";
 	int senha = lerSenha();
-	cout << "Obrigada!" << endl;
+	cout << "Obrigada!";
 
 	return 0;
+
 }
 
-void beep()
-{
+void alerta() {
 	cout << "\a";
 }
 
-int lerSenha()
-{
-	int temp;
-	cin >> temp;
-	return temp;
+int lerSenha() {
+	int senha;
+	cin >> senha;
+	return senha;
 }
